93592fa455f3182be4e21ca06c7d9618ac1c4b6c
alkhd <alkhd@LAPTOP-UIMLT75E>
2020-11-24 18:37:04 +0100

    read the local Ip Adresse and print it

---
+++ DNS Tunneling Server/DNS Tunneling Server/localIP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNS_Tunneling_Server
+{  
+      // done, still just to conect mit Gui (Lucas)
+    class localIP
+    {
+        String strHostName = string.Empty;
+        // Getting Ip address of local machine
+        // First get the host name of local machine. 
+        strHostName = Dns.GetHostName(); 
+          Console.WriteLine("Local Machine's Host Name: " + strHostName); 
+          // Then using host name, get the IP address list.. 
+         IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+        IPAddress[] addr = ipEntry.AddressList; 
+
+            for (int i = 0; i<addr.Length; i++) 
+         { 
+           Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString()); 
+           }
+
+}
+}
+  
