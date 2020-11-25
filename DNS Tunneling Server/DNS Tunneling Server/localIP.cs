b4f46473ecb98ae622670d80e22ff7142ed95fca
alkhd <alkhd@LAPTOP-UIMLT75E>
2020-11-25 11:01:45 +0100

    Fehler korregiert

--- DNS Tunneling Server/DNS Tunneling Server/localIP.cs
+++ DNS Tunneling Server/DNS Tunneling Server/localIP.cs
@@ -1,26 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
 
 namespace DNS_Tunneling_Server
-{  
-      // done, still just to conect mit Gui (Lucas)
+{
+    // done, still just to conect mit Gui (Lucas)
     class localIP
     {
-        String strHostName = string.Empty;
-        // Getting Ip address of local machine
-        // First get the host name of local machine. 
-        strHostName = Dns.GetHostName(); 
-          Console.WriteLine("Local Machine's Host Name: " + strHostName); 
-          // Then using host name, get the IP address list.. 
-         IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
-        IPAddress[] addr = ipEntry.AddressList; 
+        public getNetworkInterface()
+        {
 
-            for (int i = 0; i<addr.Length; i++) 
-         { 
-           Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString()); 
-           }
 
-}
+            String strHostName = string.Empty;
+            // Getting Ip address of local machine
+            // First get the host name of local machine. 
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            // Then using host name, get the IP address list.. 
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
+            }
+
+        }
+    }
 }
   
